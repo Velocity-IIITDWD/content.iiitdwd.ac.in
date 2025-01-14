cd4e0b7d1dd4a76cd6d1d5dd5e0b648fbc30de38
ANAS727189 <anas23khan083@gmail.com>
2025-01-14 19:58:02 +0530

    feat(feat): added schemas for 6 data such as Jobs, Member etc

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,21 +1,27 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import {facultyType} from './facultyType'
-import { About } from './About'
-import { 
-    descriptionStructure,
-    linksStructure,
-    seatStructure } from './Admission'
-import { announcementType } from './Announcement'
-import { chairPerson } from './ChairPerson'
-import { boardOfGovernor } from './BoardOfGovernor'
-import { former } from './Former'
-import { senate } from './Senate'
-import { staff } from './Staff'
-import { visitor } from './Visitor'
-import { cseDetail } from './CSEAcademics'
-import { eceDetail } from './ECEAcademics'
-import { dsaiDetail } from './DSAIAcademics'
-import { Credit } from './Credits'
+import { type SchemaTypeDefinition } from 'sanity';
+import { facultyType } from './facultyType';
+import { About } from './About';
+import {
+  descriptionStructure,
+  linksStructure,
+  seatStructure,
+} from './Admission';
+import { announcementType } from './Announcement';
+import { chairPerson } from './ChairPerson';
+import { boardOfGovernor } from './BoardOfGovernor';
+import { former } from './Former';
+import { senate } from './Senate';
+import { staff } from './Staff';
+import { visitor } from './Visitor';
+import { cseDetail } from './CSEAcademics';
+import { eceDetail } from './ECEAcademics';
+import { dsaiDetail } from './DSAIAcademics';
+import { Credit } from './Credits';
+import { Jobs } from './Jobs';
+import { Member, FacultyAdvisor, Team, Issues } from './Magazine';
+import { Club } from './Members';
+import { NIRF } from './Nirf';
+import { Tender } from './Tenders';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
@@ -35,5 +41,15 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     eceDetail,
     dsaiDetail,
     Credit,
+
+    // Updated be me
+    Jobs,
+    Member,
+    FacultyAdvisor,
+    Team,
+    Issues,
+    Club,
+    NIRF,
+    Tender,
   ],
-}
+};
